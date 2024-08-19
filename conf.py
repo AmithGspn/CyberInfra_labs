057795dc32ce283904e5074667f91d76c5f9f17e
AmithGspn <amithgspn23@gmail.com>
2024-08-19 15:52:03 -0400

    updated the test reference in lab 1

--- conf.py
+++ conf.py
@@ -13,7 +13,15 @@ author = 'Amith GSPN'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
